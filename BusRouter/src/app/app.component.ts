b283f308667da6b25cd8629af7ea6d266c2e41fe
tyler costello <tyc642@gmail.com>
2022-11-12 12:55:32 -0800

    Attempting to add map

--- BusRouter/src/app/app.component.ts
+++ BusRouter/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
 
+import {} from 'googlemaps';
+import { Component, OnInit } from '@angular/core';
+import { ViewChild } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'BusRouter';
+  @ViewChild('map') mapElement: any;
+  map: google.maps.Map;
+  ngOnInit(): void {
+    const mapProperties = {
+         center: new google.maps.LatLng(35.2271, -80.8431),
+         zoom: 15,
+         mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.mapElement.nativeElement,    mapProperties);
+ }
 }

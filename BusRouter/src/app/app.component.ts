f1a8de8e00e41e89ed139bc841b4d7739370bcb7
tyler costello <tyc642@gmail.com>
2022-11-12 13:48:57 -0800

    Map now works
    RAAAAAA

--- BusRouter/src/app/app.component.ts
+++ BusRouter/src/app/app.component.ts
@@ -1,17 +1,17 @@
 
 import {} from 'googlemaps';
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { ViewChild } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements AfterViewInit{
   title = 'BusRouter';
   @ViewChild('map') mapElement: any;
-  map: google.maps.Map;
-  ngOnInit(): void {
+  map: google.maps.Map | undefined;
+  ngAfterViewInit(): void {
     const mapProperties = {
          center: new google.maps.LatLng(35.2271, -80.8431),
          zoom: 15,

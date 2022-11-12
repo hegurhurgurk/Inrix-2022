447a3821941036376ec7efd5217d87422c79cde7
tyler costello <tyc642@gmail.com>
2022-11-12 14:48:56 -0800

    Added Markers and colors

--- BusRouter/src/app/app.component.ts
+++ BusRouter/src/app/app.component.ts
@@ -7,10 +7,15 @@ import { ViewChild } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
+
+
 export class AppComponent implements AfterViewInit{
   title = 'BusRouter';
   @ViewChild('map') mapElement: any;
   map: google.maps.Map | undefined;
+  
   ngAfterViewInit(): void {
     const mapProperties = {
          center: new google.maps.LatLng(37.7749, -122.4194),
@@ -18,5 +23,18 @@ export class AppComponent implements AfterViewInit{
          mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     this.map = new google.maps.Map(this.mapElement.nativeElement,    mapProperties);
+    this.addMarker(this.map)
+    
  }
+
+  addMarker(map: any) {
+    const marker = new google.maps.Marker({
+        position: { lat: 37.7749, lng: -122.4194},
+        icon: {
+          url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
+        },
+        map: map
+    });
+  }
 }
+ 

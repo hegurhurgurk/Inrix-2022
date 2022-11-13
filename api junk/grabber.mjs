1f79cb705708978bad11050c898a9eb6dcac6a1a
Cameron Greene <cam.greene@gmail.com>
2022-11-12 20:27:45 -0800

    made the grabber LOL
    asdf

--- api junk/grabber.mjs
+++ api junk/grabber.mjs
@@ -1,6 +1,6 @@
 //import fetch from "node-fetch";
 async function displayer(){
-  var token = await getToken()
+  var token = await getTrips()
   document.getElementById("change").innerHTML = token;
 }
 
@@ -10,32 +10,24 @@ async function getToken(){
   return json.token;
 }
 
+async function getTrips(latlong){
+  const response = await fetch("http://localhost:8000/gettrips?latlong=" + latlong);
+  const json = await response.json();
+  return json;
+}
+
+async function getPOI(){
+  var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+  
+  const response = await fetch("https://api.opentripmap.com/0.1/en/places/bbox?lon_min=-122.541&lon_max=-122.341&lat_min=37.699&lat_max=37.858&rate=2&format=json&limit=1000&apikey=5ae2e3f221c38a28845f05b6ba0fac35c7abb4081c482004315ba987", requestOptions);
+  const json = await response.json();
+  console.log(json[350].name);
+  return json;
+}
+
 (async () => {
   console.log(await getToken())
 })();
-
-async function getTrips(adskjf, asidnf){
-  let url = "skjdfhasd0" + asfja +" asdjfha";
-  const response = await fetch(url);
-  const json = await response.json();
-  
-}
-
-
-var myHeaders = new Headers();
-myHeaders.append("Authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkZXZpY2VJZCI6IjAwMDAwMDAwLTAwMDAtMDAwMC0wMDAwLTAwMDAwMDAwMDAwMCIsImNzVG9rZW4iOm51bGwsInVzZXJSb2xlIjoxLCJhcHBSb2xlIjo0LCJ1c2VySWQiOiI1MzIzY2YxNy1hNDNkLTQzZDktODRiZS1iYTRlNmU3ZmEyNDIiLCJhcHBJZCI6ImFiOGViYWQwLTQ3MGQtNDY5Zi05ODI1LWQ1YjgxNmVlZWU1YSIsImRldmVsb3BlcklkIjoiYTk0ODRiZGEtY2MxZC00YTdiLTgzMGQtYjJhNDZiYjgwY2Y3IiwiZXhwaXJ5IjoiMjAyMi0xMS0xM1QwNzozNzoyNi40OTg1NjM4WiIsImV4cCI6MTY2ODMyNTA0Niwicm9sZSI6InVzZXIifQ.Q9NKIWwmxWJ7hwbEMnF9CrJreYDGHwQDJX5A9N6_dXA");
-
-var requestOptions = {
-  method: 'GET',
-  headers: myHeaders,
-  redirect: 'follow'
-};
-
-
-function tradearea(latlong){
-
-    fetch("https://trade-areas-api.inrix.com/v1/trips?accessToken=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkZXZpY2VJZCI6IjAwMDAwMDAwLTAwMDAtMDAwMC0wMDAwLTAwMDAwMDAwMDAwMCIsImNzVG9rZW4iOm51bGwsInVzZXJSb2xlIjoxLCJhcHBSb2xlIjo0LCJ1c2VySWQiOiI1MzIzY2YxNy1hNDNkLTQzZDktODRiZS1iYTRlNmU3ZmEyNDIiLCJhcHBJZCI6ImFiOGViYWQwLTQ3MGQtNDY5Zi05ODI1LWQ1YjgxNmVlZWU1YSIsImRldmVsb3BlcklkIjoiYTk0ODRiZGEtY2MxZC00YTdiLTgzMGQtYjJhNDZiYjgwY2Y3IiwiZXhwaXJ5IjoiMjAyMi0xMS0xM1QwNzozNzoyNi40OTg1NjM4WiIsImV4cCI6MTY2ODMyNTA0Niwicm9sZSI6InVzZXIifQ.Q9NKIWwmxWJ7hwbEMnF9CrJreYDGHwQDJX5A9N6_dXA&limit=10000&geoFilterType=circle&radius=20ft&points"+ latlong +"&od=destination", requestOptions)
-      .then(response => response.text())
-      .then(result => console.log(result))
-      .catch(error => console.log('error', error));
-}

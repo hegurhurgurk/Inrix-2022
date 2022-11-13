b1c93e4a0ebfa869346148c32a14bc75bca57b90
Cameron Greene <cam.greene@gmail.com>
2022-11-12 17:22:01 -0800

    fixed the dumb shit. jordan did all of it

--- api junk/grabber.mjs
+++ api junk/grabber.mjs
@@ -1,10 +1,18 @@
-import fetch from "node-fetch";
-
-fetch('http://localhost:8000/gettoken')
-  .then((response) => response.json())
-  .then((token) => decompose(token));
-
-function decompose(inner){
-    var important = inner.token;
-    console.log(important);
+//import fetch from "node-fetch";
+async function displayer(){
+  var token = await getToken()
+  document.getElementById("change").innerHTML = token;
 }
+
+async function getToken(){
+  // fetch('http://localhost:8000/gettoken')
+  //   .then((response) => {return response.json();})
+  //   .then((json) => {token = json})
+  const response = await fetch('http://localhost:8000/gettoken');
+  const json = await response.json();
+  return json.token;
+}
+
+(async () => {
+  console.log(await getToken())
+})();
